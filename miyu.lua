742f581c21af210810ef3f9e7a1f973bb63616a4
gunblack <sargekdamianoss@gmail.com>
2025-10-21 18:24:32 +0700

    Update miyu.lua

--- miyu.lua
+++ miyu.lua
@@ -5,7 +5,7 @@ LimitTrade = 2500000
 
 HargaStallRev = 750
 HargaStallMax = 450
-HargaDeathToken = 4000
+HargaDeathToken = 3500
 
 AttackOrder5 = {1200, 1200, 1200, 1200} 
 random5 = AttackOrder5[math.random(1,#AttackOrder5)]
