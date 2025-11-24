e452c9863db30a4b7f2feffb85add407b64d2531
gunblack <sargekdamianoss@gmail.com>
2025-11-25 05:39:07 +0700

    Update miyu.lua

--- miyu.lua
+++ miyu.lua
@@ -1,4 +1,4 @@
-NamaPengumpul = {"foomsmile", "Mermaidsmist", "Fourball4", "braleigh_claire09", "Jbmc1172"}
+NamaPengumpul = {"foomsmile", "Mermaidsmist", "Fourball4", "braleigh_claire09", "Jbmc1172", "cool12jwI"}
 
 TargetShroom = 7000
 LimitTrade = 2500000 
